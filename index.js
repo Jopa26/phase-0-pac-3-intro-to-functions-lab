2d629de906f7623d370edf08e372e8a8c3597585
Luis <jqluis2011@gmail.com>
2022-03-10 17:59:38 -0800

    Completed assingment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    let a = "I can't hear you!";
+    let b = "YES INDEED!";
+    let c = "I would love to!";
+
+
+       if (string.toLowerCase(string) === string){
+        return a;        
+    } else if (string.toUpperCase(string) === string){
+        return b;
+    } else if (string === "Let's have dinner together!" ){
+        return c;
+    }
+
+   
+}
